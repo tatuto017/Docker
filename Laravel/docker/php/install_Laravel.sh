0b8dcaa3c4c26e5add38099ab07ce4b7c8368e7b
tatuto017 <tatuto017@gmail.com>
2020-06-16 08:30:16 +0900

    Laravelのインストールシェルの修正

--- Laravel/docker/php/install_Laravel.sh
+++ Laravel/docker/php/install_Laravel.sh
@@ -17,5 +17,5 @@ sed -e "s/DB_CONNECTION=mysql/DB_CONNECTION=$db/"    \
     -e "s/DB_HOST=127.0.0.1/DB_HOST=$db_host/"       \
     -e "s/DB_DATABASE=laravel/DB_DATABASE=$db_name/" \
     -e "s/DB_USERNAME=root/DB_USERNAME=$db_user/"    \
-    -e "s/DB_PASSWORD=/DB_PASSWORD==$db_pass/"       > .env.wk
+    -e "s/DB_PASSWORD=/DB_PASSWORD=$db_pass/"       > .env.wk
 mv .env.wk .env
